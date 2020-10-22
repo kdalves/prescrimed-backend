0bf2c07229874cc418ba78136c607a8bbc560451
Kathleen (Kath) Alves <49501299+kdalves@users.noreply.github.com>
2020-10-21 21:21:17 -0300

    fix: Add blank line in the end of the files

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
+
 // Update with your config settings.
 
 module.exports = {
-
-    development: {
-      client: process.env.DB_CLIENT,
-      connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
-      }
+  development: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      user:  process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT
     },
-  };
+  }
+};
