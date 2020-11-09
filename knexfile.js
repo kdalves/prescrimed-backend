4636d185747b14e2961079f9ce74a01402bdd3bf
Kathleen (Kath) Alves <49501299+kdalves@users.noreply.github.com>
2020-11-09 20:45:05 -0300

    fix: Adjusted knexfile configuration again (#10)

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,25 @@ require('dotenv').config();
 
 // Update with your config settings.
 
-module.exports = {
-  client: process.env.DB_CLIENT,
-  connection: {
-    host:  process.env.DB_HOST,
-    user:  process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+module.exports = {  
+  development: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      user:  process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT
+    },
+  },
+  production: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      user:  process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT
+    },
   }
 };
