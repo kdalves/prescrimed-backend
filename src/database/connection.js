e303bc2e263b42e39904b8d726cd75c3149729e3
Kathleen (Kath) Alves <49501299+kdalves@users.noreply.github.com>
2020-11-11 21:13:00 -0300

    fix: Rollback the connection, because its correctly (#20)

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,13 +1,6 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const dbConfig  = knex(configuration.development);
-
-var connection = mysql.createPool({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB
-});
+const connection = knex(configuration.development);
 
 module.exports = connection;
