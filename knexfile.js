575d9cd4cf304b90c44f6a7e8ce6a8302beddb8b
Kathleen (Kath) Alves <49501299+kdalves@users.noreply.github.com>
2020-11-11 22:01:00 -0300

    fix: Add database_url

--- knexfile.js
+++ knexfile.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   production: {
     client: process.env.DB_CLIENT,
-    connection: {
+    connection: process.env.DATABASE_URL ||  {
       user:  process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
