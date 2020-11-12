aef77e8c7b85de6a904c9cf85e43c8de0ad4cb57
Kathleen (Kath) Alves <49501299+kdalves@users.noreply.github.com>
2020-11-11 21:05:04 -0300

    fix: Apparently need build the connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,13 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development);
+const dbConfig  = knex(configuration.development);
+
+var connection = mysql.createPool({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB
+});
 
 module.exports = connection;
