e72f212e471f2d8b578e480b2db206ec3cbb7e7e
Kathleen (Kath) Alves <49501299+kdalves@users.noreply.github.com>
2020-11-21 22:43:45 -0300

    Novas rotas 1 (#27)
    * Feat: Added new routes to get only one item

--- src/routes.js
+++ src/routes.js
@@ -10,35 +10,35 @@ const routes = express.Router();
 
 //Usuários
 routes.get('/usuarios', UserController.index);
-routes.get('/usuarios/:id', UserController.index);
+routes.get('/usuarios/:id', UserController.update);
 routes.post('/usuarios', UserController.create);
 routes.put('/usuarios/:id', UserController.update);
 routes.delete('/usuarios/:id', UserController.delete);
 
 //Profissionais
 routes.get('/profissionais', ProfissionalController.index);
-routes.get('/profissionais/:id', ProfissionalController.index);
+routes.get('/profissionais/:id', ProfissionalController.update);
 routes.post('/profissionais', ProfissionalController.create);
 routes.put('/profissionais/:id', ProfissionalController.update);
 routes.delete('/profissionais/:id', ProfissionalController.delete);
 
 //Protocolos
 routes.get('/protocolos', ProtocolController.index);
-routes.get('/protocolos/:id', ProtocolController.index);
+routes.get('/protocolos/:id', ProtocolController.update);
 routes.post('/protocolos', ProtocolController.create);
 routes.put('/protocolos/:id', ProtocolController.update);
 routes.delete('/protocolos/:id', ProtocolController.delete);
 
 //Pacientes
 routes.get('/pacientes', PatientController.index);
-routes.get('/pacientes/:id', PatientController.index);
+routes.get('/pacientes/:id', PatientController.update);
 routes.post('/pacientes', PatientController.create);
 routes.put('/pacientes/:id', PatientController.update);
 routes.delete('/pacientes/:id', PatientController.delete);
 
 //Prescrição Médica
 routes.get('/prescricao', PrescriptionController.index);
-routes.get('/prescricao/:id', PrescriptionController.index);
+routes.get('/prescricao/:id', PrescriptionController.update);
 routes.post('/prescricao', PrescriptionController.create);
 routes.put('/prescricao/:id', PrescriptionController.update);
 routes.delete('/prescricao/:id', PrescriptionController.delete);
